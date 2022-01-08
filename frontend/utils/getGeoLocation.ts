f385d68485bd315d8256278746510807b33e7f6a
cksal0805 <valley@smartstudy.co.kr>
2022-01-08 23:13:03 +0900

    Refactor getGeoLocation promise 사용 #4

--- frontend/utils/getGeoLocation.ts
+++ frontend/utils/getGeoLocation.ts
@@ -1,16 +1,18 @@
+import { Platform } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 
 export const getGeoLocation = async () => {
-  await Geolocation.getCurrentPosition(
-    position => {
-      const latitudeToParse = JSON.stringify(position.coords.latitude);
-      const longitudeToParse = JSON.stringify(position.coords.longitude);
-      console.log(latitudeToParse, longitudeToParse);
-      return [latitudeToParse, longitudeToParse];
-    },
-    error => {
-      console.log(error.code, error.message);
-    },
-    {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-  );
+  if (Platform.OS === 'ios') {
+   await Geolocation.requestAuthorization('whenInUse');
+  }
+  return new Promise ((resolve) => {
+    Geolocation.getCurrentPosition(
+      resolve,
+      error => {
+        // TODO: 위치 불러오는 도중에 에러가 발생할 경우 유저에게 보여줄 인터페이스 설정 필요
+        console.log(error)
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  });
 };
