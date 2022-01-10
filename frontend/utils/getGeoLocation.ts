f58556a9a9580856acfc1b81467753badebaa4c6
cksal0805 <valley@smartstudy.co.kr>
2022-01-11 00:36:21 +0900

    Refactor getGeoLocation any type 변경 #4

--- frontend/utils/getGeoLocation.ts
+++ frontend/utils/getGeoLocation.ts
@@ -1,16 +1,23 @@
 import { Platform } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 
+interface IPosition {
+  latitude: number,
+  longitude: number
+}
 export const getGeoLocation = async () => {
   if (Platform.OS === 'ios') {
    await Geolocation.requestAuthorization('whenInUse');
   }
-  return new Promise<any>((resolve) => {
+  return new Promise<IPosition>((resolve, reject) => {
     Geolocation.getCurrentPosition(
-      resolve,
+      position => {
+        const {latitude, longitude} = position.coords;
+        resolve({latitude, longitude})
+      },
       error => {
         // TODO: 위치 불러오는 도중에 에러가 발생할 경우 유저에게 보여줄 인터페이스 설정 필요
-        console.log(error)
+        reject(error)
       },
       {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
     );
